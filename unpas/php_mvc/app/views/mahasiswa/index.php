97288ed4c1eb1fe4b4981f36247ac66aed7b2854
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2021-06-01 14:09:59 +0700

    delete

--- unpas/php_mvc/app/views/mahasiswa/index.php
+++ unpas/php_mvc/app/views/mahasiswa/index.php
@@ -1,6 +1,12 @@
 <div class="container mt-3">
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
             <!-- Button trigger modal -->
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
                 Tambah Data Mahasiswa
@@ -9,9 +15,12 @@
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
+                <li class="list-group-item">
                     <?= $mhs['nama']; ?>
-                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary">Detail</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>"
+                        class="badge badge-danger float-right ml-1" onclick="return confirm ('yakin?')">Hapus</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>"
+                        class="badge badge-primary float-right ml-2">Detail</a>
                 </li>
                 <?php endforeach; ?>
             </ul>
